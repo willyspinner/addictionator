f2d0fdb2ddea43ce0a122fac95f2190ff4640447
willyspinner <wilsonjusuf1998@gmail.com>
2021-02-07 02:00:49 -0800

    add twilio call functionality

--- twilio_call.py
+++ twilio_call.py
@@ -1,16 +1,22 @@
 import os
+import urllib
 from twilio.rest import Client
 
 # Your Account Sid and Auth Token from twilio.com/console
 # and set the environment variables. See http://twil.io/secure
 account_sid = os.environ['TWILIO_ACCOUNT_SID']
 auth_token = os.environ['TWILIO_AUTH_TOKEN']
+twilio_phone_number = os.environ['TWILIO_PHONE_NUMBER']
 client = Client(account_sid, auth_token)
 
 def do_remind_call(phone_number):
+  # see this: https://www.twilio.com/labs/twimlets/echo
+  with open('./remind_voice_call.xml') as f:
+    remind_voice_call_xml = urllib.parse.quote_plus(f.read())
+  
   call = client.calls.create(
-                          url='https://raw.githubusercontent.com/willyspinner/addictionator/master/remind_voice_call.xml',
+                          url=f'http://twimlets.com/echo?Twiml={remind_voice_call_xml}',
                           to=phone_number,
-                          from_='+15017122661'
+                          from_=twilio_phone_number
                       )
     
